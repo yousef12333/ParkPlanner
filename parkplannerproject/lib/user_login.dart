133c0d9bc55d5ad212be4a47bf7e41871736af08
Youssef <s129059@ap.be>
2023-03-30 15:05:47 +0200

    database fetch en validatie gelukt + copy paste werk user_registration naar login

--- parkplannerproject/lib/user_login.dart
+++ parkplannerproject/lib/user_login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -10,9 +11,192 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  void _signIn() async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+
+      final user = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      if (!mounted) return;
+      Navigator.of(context).pushReplacementNamed('/home');
+    } on FirebaseAuthException catch (error) {
+      String message = '';
+      if (error.code == 'user-not-found') {
+        message =
+            'Gebruiker niet gevonden. Controleer uw gegevens en probeer het opnieuw.';
+      } else if (error.code == 'wrong-password') {
+        message =
+            'Verkeerde password. Controleer uw gegevens en probeer het opnieuw.';
+      } else {
+        message =
+            error.message ?? 'Er is een fout opgetreden. Probeer het opnieuw.';
+      }
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Error'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(ctx).pop();
+              },
+              child: const Text('OK'),
+            )
+          ],
+        ),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        body: _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                SizedBox(
+                  height: MediaQuery.of(context).padding.top,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color.fromARGB(255, 222, 222, 222)
+                            .withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  margin: EdgeInsets.zero,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 120,
+                        height: 22,
+                        child: Image.asset(
+                          '/ParkPlannerLogo.png',
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        '    Log in',
+                        style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            TextFormField(
+                              controller: _emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: const InputDecoration(
+                                labelText: 'Email',
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty || !value.contains('@')) {
+                                  return 'Geef alstublieft een correcte email op.';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(
+                              height: 16.0,
+                            ),
+                            TextFormField(
+                              controller: _passwordController,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                labelText: 'Wachtwoord',
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty || value.length < 7) {
+                                  return 'Geef een wachtwoord met minstens 7 karakters op.';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 20),
+                            ElevatedButton(
+                              onPressed: _signIn,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.green,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                              child: const Text(
+                                'Inloggen',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            GestureDetector(
+                              child: RichText(
+                                text: TextSpan(
+                                  text: 'Heb je nog geen account? ',
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                  ),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: 'Creëer je account',
+                                      style: const TextStyle(
+                                        color: Colors.green,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () {
+                                          Navigator.of(context)
+                                              .pushNamed('/register');
+                                        },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ]));
   }
 }
