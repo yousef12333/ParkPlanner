c76c6f5ec39b20ee20540ef3b681c87bb142b4e6
Youssef <s129059@ap.be>
2023-05-03 16:56:02 +0200

    layout verbeterd

--- parkplannerproject/lib/edit_password.dart
+++ parkplannerproject/lib/edit_password.dart
@@ -86,87 +86,142 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Wachtwoord aanpassen'),
-        ),
         body: _isLoading
             ? const Center(
                 child: CircularProgressIndicator(),
               )
-            : Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    //mainAxisAlignment: MainAxisAlignment.center,
-                    // crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      TextFormField(
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black)),
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty || !value.contains('@')) {
-                            return 'Geef alstublieft een correcte email op.';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 16),
-                      TextFormField(
-                        controller: _oldPasswordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelText: 'Oud wachtwoord',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black)),
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty || value.length < 7) {
-                            return 'Geef een wachtwoord met minstens 7 karakters op.';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 16),
-                      TextFormField(
-                        controller: _newPasswordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          labelText: 'Nieuw wachtwoord',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black)),
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty || value.length < 7) {
-                            return 'Geef een wachtwoord met minstens 7 karakters op.';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: _changePassword,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                        child: const Text(
-                          'Aanpassen',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).padding.top,
                   ),
-                ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 222, 222, 222)
+                              .withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    margin: EdgeInsets.zero,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 120,
+                          height: 22,
+                          child: Image.asset(
+                            '/ParkPlannerLogo.png',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        const Text(
+                          '    Wachtwoord aanpassen',
+                          style: TextStyle(
+                            fontSize: 19,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green,
+                          ),
+                        ),
+                        Expanded(
+                          child: InkWell(
+                            child: IconButton(
+                              alignment: Alignment.centerRight,
+                              icon: const Icon(Icons.keyboard_return_rounded),
+                              color: Colors.green,
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/login');
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Center(
+                      child: SingleChildScrollView(
+                          child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black)),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty || !value.contains('@')) {
+                                return 'Geef alstublieft een correcte email op.';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _oldPasswordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              labelText: 'Oud wachtwoord',
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black)),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty || value.length < 7) {
+                                return 'Geef een wachtwoord met minstens 7 karakters op.';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _newPasswordController,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              labelText: 'Nieuw wachtwoord',
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black)),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty || value.length < 7) {
+                                return 'Geef een wachtwoord met minstens 7 karakters op.';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: _changePassword,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            child: const Text(
+                              'Aanpassen',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )))
+                ],
               ));
   }
 }
